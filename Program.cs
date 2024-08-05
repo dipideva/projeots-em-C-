83502a52e072def74d1ef0981e4dd383085787c3
dipideva <diegodevalves@gmail.com>
2024-08-05 14:03:03 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,79 +1,150 @@
-﻿using System;
 using System.Collections.Generic;
-
-class Cliente
+class cliente
 {
-    public string Nome { get; set; }
-    public string Email { get; set; }
+    public string nome;
+    public string email;
+
 }
-
-class Compra
+class compra
 {
-    public void AdicionarItens(int opcaoDeCompra)
+    private int OPC;
+
+    public void opc(int OPC)
     {
+        int cont = 1;
         List<float> carrinho = new List<float>();
         List<string> opctam = new List<string>();
-
-        float[] precosCamisetas = { 20.5f, 30.5f, 30.5f, 40.0f, 70.0f };
-
-        while (opcaoDeCompra == 1)
+        float camiseta_1 = 20.5f;
+        float camiseta_2 = 30.5f;
+        float camiseta_3 = 30.5f;
+        float camiseta_4 = 40.0f;
+        float camiseta_5 = 70.0f;
+        if (OPC == 1)
         {
-            Console.WriteLine("Qual deseja comprar 1, 2, 3, 4, ou 5?");
-            int opcaoCamiseta = int.Parse(Console.ReadLine());
-            if (opcaoCamiseta < 1 || opcaoCamiseta > 5)
+            while (cont == 1)
             {
-                Console.WriteLine("Valor inválido. Escolha o número da camiseta equivalente aos números fornecidos como opções.");
-                continue;
-            }
-
-            Console.WriteLine($"A camiseta {opcaoCamiseta} possui os tamanhos P, M, G, GG e EGG. Qual tamanho você deseja?");
-            string tamanho = Console.ReadLine();
-
-            carrinho.Add(precosCamisetas[opcaoCamiseta - 1]);
-            opctam.Add(tamanho);
-
-            Console.WriteLine($"A camiseta {opcaoCamiseta}, tamanho {tamanho}, foi adicionada ao carrinho.");
-            Console.WriteLine("Você deseja escolher outra camisa? 1 - Sim // 2 - Não");
-            int resposta = int.Parse(Console.ReadLine());
-            if (resposta != 1)
-            {
-                break;
+                Console.WriteLine("Qual deseja comprar 1,2,3,4, ou 5 ?");
+                int opc1 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"A camiseta {opc1} possui os tamanhos P,M,G,GG e EGG. Qual tamanho você deseja ?");
+                string opctamanho = Console.ReadLine();
+                if (opc1 == 1)
+                {
+                    carrinho.Add(camiseta_1);
+                    opctam.Add(opctamanho);
+                    Console.WriteLine($"A camiseta 1, tamanho {opctamanho} foi adicionada ao carrinho foi adicionado ao carrinho");
+                    Console.WriteLine("Você deseja escolher outra camisa ? 1 - Sim // 2 - Não");
+                    int resp1 = int.Parse(Console.ReadLine());
+                    if (resp1 == 1)
+                    {
+                        cont = cont + 1;
+                    }
+                    else
+                    {
+                        cont = cont - 1;
+                        break;
+                    }
+                }
+                else if (opc1 == 2)
+                {
+                    carrinho.Add(camiseta_2);
+                    Console.WriteLine($"A camiseta 2, tamanho {opctamanho} foi adicionada ao carrinho foi adicionado ao carrinho");
+                    Console.WriteLine("Você deseja escolher outra camisa ? 1 - Sim // 2 - Não");
+                    int resp2 = int.Parse(Console.ReadLine());
+                    if (resp2 == 1)
+                    {
+                        cont = cont + 1;
+                    }
+                    else
+                    {
+                        cont = cont - 1;
+                        break;
+                    }
+                }
+                else if (opc1 == 3)
+                {
+                    carrinho.Add(camiseta_3);
+                    Console.WriteLine($"A camiseta 3, tamanho {opctamanho} foi adicionada ao carrinho foi adicionado ao carrinho");
+                    Console.WriteLine("Você deseja escolher outra camisa ? 1 - Sim // 2 - Não");
+                    int resp3 = int.Parse(Console.ReadLine());
+                    if (resp3 == 1)
+                    {
+                        cont = cont + 1;
+                    }
+                    else
+                    {
+                        cont = cont - 1;
+                        break;
+                    }
+                }
+                else if (opc1 == 4)
+                {
+                    carrinho.Add(camiseta_4);
+                    Console.WriteLine($"A camiseta 4, tamanho {opctamanho} foi adicionada ao carrinho foi adicionado ao carrinho");
+                    Console.WriteLine("Você deseja escolher outra camisa ? 1 - Sim // 2 - Não");
+                    int resp4 = int.Parse(Console.ReadLine());
+                    if (resp4 == 1)
+                    {
+                        cont = cont + 1;
+                    }
+                    else
+                    {
+                        cont = cont - 1;
+                        break;
+                    }
+                }
+                else if (opc1 == 5)
+                {
+                    carrinho.Add(camiseta_5);
+                    Console.WriteLine($"A camiseta 5, tamanho {opctamanho} foi adicionada ao carrinho foi adicionado ao carrinho");
+                    Console.WriteLine("Você deseja escolher outra camisa ? 1 - Sim // 2 - Não");
+                    int resp5 = int.Parse(Console.ReadLine());
+                    if (resp5 == 1)
+                    {
+                        cont = cont + 1;
+                    }
+                    else
+                    {
+                        cont = cont - 1;
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Valor invalido. escolha o numero da camiseta equivalente aos numeros fornecidos como opçôes. ");
+                }
             }
         }
+        else { Console.WriteLine("Sua compra foi finalizada"); }
     }
-}
 
-class Program
-{
-    public static void Main(string[] args)
+
+    class Program
     {
-        Console.WriteLine("Digite seu nome:");
-        string nome = Console.ReadLine();
-        Console.WriteLine($"Bem vindo {nome}! Esta é a melhor loja de camisetas.");
-        Console.WriteLine();
-        Console.WriteLine("Deseja ver nosso catálogo de camisetas? 1 - Sim // 2 - Não");
-        int resp = int.Parse(Console.ReadLine());
-
-        while (resp == 1)
+        public static void Main(string[] args)
         {
-            Console.WriteLine(
-                "Camiseta 1 R$ 20,5\n" +
-                "Camiseta 2 R$ 30,5\n" +
-                "Camiseta 3 R$ 30,5\n" +
-                "Camiseta 4 R$ 40,0\n" +
-                "Camiseta 5 R$ 70,0\n"
-            );
 
-            Compra compra = new Compra();
+            Console.WriteLine("Digite seu nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Bem vindo {nome}! Esta é a melhor loja de camisetas.");
+            Console.WriteLine();
+            Console.WriteLine("Deseja ver nosso catalogo de camisetas? 1 - Sim // 2 - Não");
+            int resp = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Deseja comprar alguma camiseta? 1 - Sim // 2 - Não ");
-            int opc = int.Parse(Console.ReadLine());
-            compra.AdicionarItens(opc);
+            while (resp == 1)
+            {
 
-            Console.WriteLine("Deseja continuar vendo o catálogo? 1 - Sim // 2 - Não");
-            resp = int.Parse(Console.ReadLine());
-        }
+                Console.WriteLine(
+                    "Camiseta 1 R$ 20,5 " +
+                    "Camiseta 2 R$ 30,5" +
+                    "Camiseta 3 R$ 30,5" +
+                    "Camiseta 4 R$ 40,0" +
+                    "Camiseta 5 R$ 70,0");
 
-        Console.WriteLine("Sua compra foi finalizada. Obrigado!");
-    }
-}
+
+                compra c = new compra();
+
+                Console.WriteLine("Deseja comprar alguma camiseta ? 1 - Sim // 2 - Não ");
+                int opc = int.Parse(Console.ReadLine());
+                c.opc(opc);
+
+            }
